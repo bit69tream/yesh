d3c894df9b44569561be36f6d1204c9abf193b02
bit6tream <megapixel483@gmail.com>
2023-05-07 01:23:00 +0300

    Add basic prompt movement and editing capabilities

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use gettextrs::{setlocale, LocaleCategory};
 use ncursesw::*;
 use std::panic;
 
-struct Yesh {
-    size: Size,
+struct Yesh<'a> {
+    window_size: Size,
     window: WINDOW,
     command: Vec<WideChar>,
     attributes: normal::Attributes,
     color_pair: normal::ColorPair,
+    prompt: &'a str,
+    cursor_position: Origin,
 }
 
-impl Yesh {
+impl Yesh<'_> {
     fn new() -> Result<Self, ncursesw::NCurseswError> {
         // TODO: also handle ctrl-d (EINTR)
         ctrlc::set_handler(|| {}).expect("cannot set ctrl-c handler");
@@ -29,29 +31,74 @@ impl Yesh {
             }
         };
 
+        let prompt = "% ";
         let yesh = Yesh {
             window,
-            size: getmaxyx(window)?,
+            window_size: getmaxyx(window)?,
             command: Vec::new(),
             attributes,
             color_pair,
+            prompt,
+            cursor_position: Origin { x: prompt.len() as i32, y: 0 },
         };
         Ok(yesh)
     }
 
     fn process_key(&mut self, key: KeyBinding) -> Result<(), ncursesw::NCurseswError> {
+        use ncursesw::KeyBinding::*;
+
+        match key {
+            Backspace => {
+                self.cursor_position.x -= 1;
+                self.command.pop();
+            }
+            DeleteCharacter => {
+                let index = self.cursor_position.x as usize - self.prompt.len();
+
+                if (index < self.command.len()) {
+                    self.command.remove(index);
+                }
+            }
+            LeftArrow => {
+                self.cursor_position.x = (self.cursor_position.x - 1).clamp(self.prompt.len() as i32, (self.command.len() + self.prompt.len()) as i32);
+            }
+            RightArrow => {
+                self.cursor_position.x = (self.cursor_position.x + 1).clamp(self.prompt.len() as i32, (self.command.len() + self.prompt.len()) as i32);
+            }
+            _ => {}
+        }
+
         Ok(())
     }
 
+    fn process_control_characters(&mut self, control_character: char) {
+        use ascii::{AsciiChar, ToAsciiChar};
+
+        match control_character.to_ascii_char().unwrap() {
+            AsciiChar::LineFeed => {
+                todo!()
+            }
+
+            // NOTE: for some reason pressing backspace produces DEL. actual delete key is processed in `process_key`
+            AsciiChar::BackSpace | AsciiChar::DEL => {
+                self.command.pop();
+                self.cursor_position.x -= 1;
+            }
+            _ => {}
+        }
+    }
+
     fn process_character(&mut self, character: WideChar) -> Result<(), ncursesw::NCurseswError> {
-        if let Ok(value) = character.as_char() {
-            if value.is_control() {
+        if let Ok(control_character) = character.as_char() {
+            if control_character.is_control() {
+                self.process_control_characters(control_character);
                 return Ok(());
             }
         }
-        self.command.push(character);
 
-        // wadd_wch(self.window, ComplexChar::from_wide_char(character, &self.attributes, &self.color_pair)?)?;
+        self.command.push(character);
+        self.cursor_position.x += 1;
+
         Ok(())
     }
 
@@ -71,16 +118,18 @@ impl Yesh {
         wclear(self.window);
 
         wmove(self.window, Origin::default())?;
-        waddstr(self.window, "%> ");
+        waddstr(self.window, self.prompt);
         for character in &self.command {
             wadd_wch(self.window, ComplexChar::from_wide_char(*character, &self.attributes, &self.color_pair)?)?;
         }
+
+        wmove(self.window, self.cursor_position);
 
         wrefresh(self.window)
     }
 }
 
-impl Drop for Yesh {
+impl Drop for Yesh<'_> {
     fn drop(&mut self) {
         close_ncurses_window();
     }
