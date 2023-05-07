52ee3fdc4e0e65f79c704f9df8ff77a13c02563b
bit6tream <megapixel483@gmail.com>
2023-05-07 22:21:45 +0300

    Rendering system rewrite

--- src/yesh.rs
+++ src/yesh.rs
@@ -101,6 +101,57 @@ impl Yesh<'_> {
         self.rebuild_line_views();
         Ok(())
     }
+
+    fn rebuild_line_views(&mut self) {
+        self.line_views.clear();
+
+        let mut y = 0i32;
+        for (index, line) in self.lines.iter().enumerate() {
+            let mut remaining_width = line.len() as i32;
+            let mut offset: usize = 0;
+
+            loop {
+                let view_width = self.window_size.columns.min(remaining_width);
+                self.line_views.push(LineView { y, width: view_width, index, offset });
+
+                y += 1;
+                offset += (view_width - 1) as usize;
+                remaining_width -= view_width;
+
+                if remaining_width <= 0 {
+                    break;
+                }
+            }
+        }
+    }
+
+    fn rebuild_command_views(&mut self) {
+        self.command_views.clear();
+
+        let mut y: i32 = if self.line_views.len() > 0 { self.line_views[self.line_views.len() - 1].y + 1 } else { 0 };
+        let mut remaining_width: i32 = self.command.len() as i32;
+        let mut offset: usize = 0;
+
+        loop {
+            let max_view_width = if (self.command_views.len() == 0) {
+                self.window_size.columns - self.prompt.len() as i32
+            } else {
+                self.window_size.columns
+            };
+            let view_width = remaining_width.min(max_view_width);
+
+            self.command_views.push(CommandView { y, width: view_width, offset });
+
+            y += 1;
+            offset += (view_width - 1) as usize;
+            remaining_width -= view_width;
+
+            if (remaining_width <= 0) {
+                break;
+            }
+        }
+    }
+
     fn process_key(&mut self, key: KeyBinding) -> Result<(), ncursesw::NCurseswError> {
         use ncursesw::KeyBinding::*;
 
@@ -128,15 +179,23 @@ impl Yesh<'_> {
                 self.cursor_position.x += 1;
             }
 
+            UpArrow => {
+                self.cursor_position.y -= 1;
+            }
+
+            DownArrow => {
+                self.cursor_position.y += 1;
+            }
+
             ResizeEvent => {
                 self.handle_resize()?;
             }
+
             _ => {}
         }
 
         Ok(())
     }
-
 
     fn execute_command(&mut self) -> Result<(), NCurseswError> {
         // let command = self.into_command();
@@ -151,10 +210,13 @@ impl Yesh<'_> {
             prompt_line.push(ComplexChar::from_wide_char(*character, &self.attributes, &self.color_pair)?);
         }
         self.command.clear();
+        self.command_views.clear();
 
         self.cursor_position.y += 1;
+        self.cursor_position.x = self.prompt.len() as i32;
 
         self.lines.push(prompt_line);
+        self.rebuild_line_views();
 
         Ok(())
     }
@@ -184,9 +246,21 @@ impl Yesh<'_> {
             // NOTE: for some reason pressing backspace produces DEL. actual delete key is processed in `process_key`
             AsciiChar::BackSpace | AsciiChar::DEL => {
                 self.command.pop();
-                self.cursor_position.x -= 1;
+                self.advance_cursor_left();
             }
             _ => {}
+        }
+    }
+
+    fn advance_cursor_left(&mut self) {
+        self.cursor_position.x -= 1;
+    }
+
+    fn advance_cursor_right(&mut self) {
+        self.cursor_position.x += 1;
+        if self.cursor_position.x >= self.window_size.columns as i32 {
+            self.cursor_position.y += 1;
+            self.cursor_position.x = 0;
         }
     }
 
@@ -199,13 +273,16 @@ impl Yesh<'_> {
         }
 
         self.command.push(character);
-        self.cursor_position.x += 1;
+        self.advance_cursor_right();
+        self.rebuild_command_views();
 
         Ok(())
     }
 
     fn clamp_cursor(&mut self) {
-        self.cursor_position.x = self.cursor_position.x.clamp(self.prompt.len() as i32, (self.command.len() + self.prompt.len()) as i32);
+        let maximum_allowed_x = (self.command.len() + self.prompt.len()).min((self.window_size.columns - 1) as usize);
+
+        self.cursor_position.x = self.cursor_position.x.clamp(0, maximum_allowed_x as i32);
         self.cursor_position.y = self.cursor_position.y.clamp(0, self.window_size.lines as i32);
     }
 
@@ -217,7 +294,7 @@ impl Yesh<'_> {
                 Key(key) => self.process_key(key)?,
                 Character(character) => self.process_character(character)?,
             }
-        } else if self.semaphore.load(Ordering::Relaxed) {
+        } else if self.control_c_semaphore.load(Ordering::Relaxed) {
             use ascii::AsciiChar;
 
             self.process_control_character(AsciiChar::ETX.as_char());
@@ -228,22 +305,49 @@ impl Yesh<'_> {
         return Ok(self.should_exit);
     }
 
-    pub fn render(&self) -> Result<(), ncursesw::NCurseswError> {
-        use ascii::AsciiChar;
+    fn is_y_on_screen(&self, y: i32) -> bool {
+        y >= self.scroll_offset && y < (self.scroll_offset + self.window_size.lines)
+    }
 
-        wclear(self.window)?;
-
-        wmove(self.window, Origin::default())?;
-        for line in &self.lines {
-            for character in line {
-                wadd_wch(self.window, *character)?;
+    fn render_lines(&self, y: i32) -> Result<i32, ncursesw::NCurseswError> {
+        let mut y = y;
+        let mut drawn_something = false;
+        for view in &self.line_views {
+            if !self.is_y_on_screen(view.y) {
+                continue;
             }
-            waddch(self.window, ChtypeChar::new(AsciiChar::LineFeed))?;
+
+            drawn_something = true;
+            y = view.y - self.scroll_offset;
+            wmove(self.window, Origin { x: 0, y: y })?;
+            for i in 0..view.width as usize {
+                wadd_wch(self.window, self.lines[view.index][view.offset + i])?;
+            }
         }
 
+        if drawn_something {
+            y += 1;
+        }
+
+        Ok(y)
+    }
+
+    fn render_command(&self, y: i32) -> Result<(), ncursesw::NCurseswError> {
+        wmove(self.window, Origin { x: 0, y });
         waddstr(self.window, self.prompt)?;
-        for character in &self.command {
-            wadd_wch(self.window, ComplexChar::from_wide_char(*character, &self.attributes, &self.color_pair)?)?;
+
+        for view in &self.command_views {}
+
+        Ok(())
+    }
+
+    pub fn render(&self) -> Result<(), ncursesw::NCurseswError> {
+        wclear(self.window)?;
+
+        let next_y = self.render_lines(0)?;
+
+        if self.is_y_on_screen(next_y) {
+            self.render_command(next_y)?;
         }
 
         wmove(self.window, self.cursor_position)?;
